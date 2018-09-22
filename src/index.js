c66e38b3425a26723b6b8c1523b042c17219b2dd
Катрин Райлян <k.shalamowa@gmail.com>
2018-09-22 13:13:36 +0300

    Implemented solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var brackets = "[]{}()",
+
+    bracketsConfig = [],
+    i, object, bracketsLocation;
+
+  for (i = 0; object = str[i]; i++) {
+
+    bracketsLocation = brackets.indexOf(object);
+    if (bracketsLocation === -1) {
+      continue;
+    }
+
+    if (bracketsLocation % 2 === 0) {
+      bracketsConfig.push(bracketsLocation + 1);
+    } else {
+      if (bracketsConfig.pop() !== bracketsLocation) {
+        return false;
+      }
+    }
+  }
+  return bracketsConfig.length === 0;
 }
