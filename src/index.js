b0c35196fb599fd9e5091450d7c5b6b8df391585
Катрин Райлян <k.shalamowa@gmail.com>
2018-09-22 15:55:25 +0300

    Update: fixed double false

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  var brackets = "[]{}()",
-
-    bracketsConfig = [],
-    i, object, bracketsLocation;
-
-  for (i = 0; object = str[i]; i++) {
-
-    bracketsLocation = brackets.indexOf(object);
-    if (bracketsLocation === -1) {
-      continue;
+  var allBrackets = "()[]{}",
+      bracketsConfig = [];
+      str = str.split('')
+      
+  for (var i = 0, thing; thing = str[i]; i++) {
+    var searchString = allBrackets.indexOf(thing); //finds iteration(s) in bracketsConfig
+    if (searchString === -1) { // if not found
+      continue; // goes to next iteration
     }
-
-    if (bracketsLocation % 2 === 0) {
-      bracketsConfig.push(bracketsLocation + 1);
+    if (searchString % 2 === 0) { 
+        bracketsConfig.push(searchString + 1); 
+        // adds # of arrays depending on index of allBrackets
     } else {
-      if (bracketsConfig.pop() !== bracketsLocation) {
-        return false;
+      if (bracketsConfig.pop() !== searchString) {
+        //console.log(false);
       }
     }
   }
-  return bracketsConfig.length === 0;
+return bracketsConfig.length === 0;
 }
