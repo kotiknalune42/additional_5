5c238470d385161b2339dc65976e9d9f1f6200fc
Катрин Райлян <k.shalamowa@gmail.com>
2018-09-22 16:48:13 +0300

    please work

--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ module.exports = function check(str, bracketsConfig) {
       str = str.split('')
       
   for (var i = 0, thing; thing = str[i]; i++) {
+
     var searchString = allBrackets.indexOf(thing); //finds iteration(s) in bracketsConfig
     if (searchString === -1) { // if not found
       continue; // goes to next iteration
     }
+
     if (searchString % 2 === 0) { 
         bracketsConfig.push(searchString + 1); 
         // adds # of arrays depending on index of allBrackets
     } else {
       if (bracketsConfig.pop() !== searchString) {
-        //console.log(false);
-      }
+        return false;
+      } else {
+       return bracketsConfig.length == 0;
+      } 
     }
   }
-return bracketsConfig.length === 0;
 }
