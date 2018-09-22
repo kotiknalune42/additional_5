3ba065cc6adad29fddcdedf27481f77307484480
Катрин Райлян <k.shalamowa@gmail.com>
2018-09-22 16:49:54 +0300

    reverted

--- src/index.js
+++ src/index.js
@@ -4,21 +4,18 @@ module.exports = function check(str, bracketsConfig) {
       str = str.split('')
       
   for (var i = 0, thing; thing = str[i]; i++) {
-
     var searchString = allBrackets.indexOf(thing); //finds iteration(s) in bracketsConfig
     if (searchString === -1) { // if not found
       continue; // goes to next iteration
     }
-
     if (searchString % 2 === 0) { 
         bracketsConfig.push(searchString + 1); 
         // adds # of arrays depending on index of allBrackets
     } else {
       if (bracketsConfig.pop() !== searchString) {
-        return false;
-      } else {
-       return bracketsConfig.length == 0;
-      } 
+        //console.log(false);
+      }
     }
   }
+return bracketsConfig.length === 0;
 }
