08158c79041dab66505dc1645de81e3e3e8996d8
Катрин Райлян <k.shalamowa@gmail.com>
2018-09-30 22:34:21 +0300

    Updated code

--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  var tempArray = [],
-    lastArray,
-    bracketStr = String(bracketsConfig).split(',');
+let temporaryArray = [], checker,
+    bracketString = String(bracketsConfig).split(','),
+    len = str.length, temporaryLen = temporaryArray.length;
 
-  for (var i = 0; i < str.length; i++) {
-   
-    for (var j = 0; j < bracketStr.length; j += 2) {
-
-      if (str[i] == bracketStr[j + 1] && lastArray == bracketStr[j]) {
-        tempArray.pop();
-        lastArray = tempArray[tempArray.length - 1];
-      
-      } else if (str[i] == bracketStr[j]) {
-        tempArray.push(bracketStr[j]);
-        lastArray = tempArray[tempArray.length - 1];
+  for (let i = 0; i < len; i++) {
+   for (let j = 0; j < bracketString.length; j += 2) {
+      if (str[i] == bracketString[j + 1] && checker == bracketString[j]) {
+        temporaryArray.pop();
+        checker = temporaryArray[temporaryLen - 1];
+     
+      } else if (str[i] == bracketString[j]) {
+        temporaryArray.push(bracketString[j]);
+        checker = temporaryArray[temporaryLen - 1];
         break;
       }
-
     }
-  }
-  if (str.length % 2) {
-    return false;
-  } else if (tempArray.length == 0) {
-    return true; 
-  } else return false;
+  } 
+return temporaryLen == 0
 }
