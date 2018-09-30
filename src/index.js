532a1769c676317d3ea94af39cbb91d706ac2ffa
Катрин Райлян <k.shalamowa@gmail.com>
2018-09-30 23:24:44 +0300

    Shorter implementation

--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-let temporaryArray = [], checker,
-    bracketString = String(bracketsConfig).split(','),
-    len = str.length, temporaryLen = temporaryArray.length;
-
+let len = str.length;
+if (len % 2 !== 0) {
+    return false;
+  }
+let temporary = [];
+mask = str.split('');
+    
   for (let i = 0; i < len; i++) {
-   for (let j = 0; j < bracketString.length; j += 2) {
-      if (str[i] == bracketString[j + 1] && checker == bracketString[j]) {
-        temporaryArray.pop();
-        checker = temporaryArray[temporaryLen - 1];
-     
-      } else if (str[i] == bracketString[j]) {
-        temporaryArray.push(bracketString[j]);
-        checker = temporaryArray[temporaryLen - 1];
-        break;
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][1] && temporary[temporary.length - 1] == bracketsConfig[j][0]) {
+        temporary.pop();
+      } else if (mask[i] === bracketsConfig[j][0]) {
+        temporary.push(mask[i]);
       }
-
     }
   }
-  
-if (str.length % 2) {
-    return false;
-  } else {
-    return temporaryLen == 0;
-}
+ return (stack.length == 0);
 }
